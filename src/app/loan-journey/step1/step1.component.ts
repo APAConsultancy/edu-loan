34c2c3ec13a544ff1dc9e0c7048070e10e2f9600
Ashish Ranjan <ashish2013@hotmail.com>
2025-02-05 17:31:52 +0530

    Step1 selections changes

--- src/app/loan-journey/step1/step1.component.ts
+++ src/app/loan-journey/step1/step1.component.ts
@@ -1,27 +1,59 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, ValidationErrors, Validators } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
+import { NgOtpInputModule } from 'ng-otp-input';
+import { Router } from '@angular/router';
 import { OtpPopupComponent } from '../../common/otp-popup/otp-popup.component';
 
 @Component({
   selector: 'app-step1',
-  imports: [ReactiveFormsModule, FormsModule, CommonModule],
+  imports: [ReactiveFormsModule, FormsModule, CommonModule, NgOtpInputModule],
   templateUrl: './step1.component.html',
   styleUrl: './step1.component.css'
 })
 export class Step1Component  implements OnInit {
   public stepOneForm: FormGroup;
+  otp: string | undefined;
+  showOtpComponent = false;
+  showMobileComponent = true;
+  correctOtp = '12345'; // Example correct OTP for validation
 
   constructor(
     private fb: FormBuilder,
-    public dialog: MatDialog
-
-
+    public dialog: MatDialog,
+    private router: Router
   ) {
     this.stepOneForm = this.fb.group({
-      mobileno: this.fb.control('', Validators.required)
+      //mobileno: this.fb.control('', Validators.required)
+      mobileno: ['', [Validators.required, Validators.pattern(/^\d{10}$/), this.onlyDigitsValidator]]
     });
+  }
+
+  @ViewChild('ngOtpInput', { static: false}) ngOtpInput: any;
+  config = {
+    allowNumbersOnly: false,
+    length: 5,
+    isPasswordInput: false,
+    disableAutoFocus: false,
+    placeholder: '',
+    inputStyles: {
+      'width': '50px',
+      'height': '50px'
+    }
+  };
+  onOtpChange(otp: string): void {
+    this.otp = otp;
+    if (otp.length === this.config.length) {
+      this.validateOtp();
+    }
+  }
+  validateOtp(): void {
+    if (this.otp === this.correctOtp) {
+      this.showMobileComponent = false;
+    } else {
+      alert('Invalid OTP. Please try again.');
+    }
   }
 
   ngOnInit(): void {
@@ -30,9 +62,34 @@ export class Step1Component  implements OnInit {
   stepOneSubmit() {
   }
 
-  openOtpPopup(): void {
-    this.dialog.open(OtpPopupComponent, {
-      width: '250px'
-    });
+  validateAndOpenOtpPopup(): void {
+    if (this.stepOneForm.controls['mobileno'].valid) {
+      //this.openOtpPopup();
+      this.showOtpComponent = true;
+    } else {
+      //alert('Please enter a valid mobile number.');
+    }
+  }
+  // openOtpPopup(): void {
+  //   this.dialog.open(OtpPopupComponent, {
+  //     width: '250px'
+  //   });
+  // }
+
+  onlyDigitsValidator(control: AbstractControl): ValidationErrors | null {
+    const value = control.value;
+    if (/^\d+$/.test(value)) {
+      return null;
+    } else {
+      return { invalidNumber: true };
+    }
+  }
+
+  goToStep2(): void {
+    if (this.stepOneForm.controls['source'].valid) {
+      this.router.navigate(['/step2']);
+    } else {
+      alert('Please select an option.');
+    }
   }
 }
