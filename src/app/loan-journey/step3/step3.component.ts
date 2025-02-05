b7bbf472f99a53438664b78823655149e6ed6ab8
piyushashu <piyush976156@gmail.com>
2025-02-05 20:31:49 +0530

    create step 3

--- src/app/loan-journey/step3/step3.component.ts
+++ src/app/loan-journey/step3/step3.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-step3',
-  imports: [],
+  imports: [CommonModule,ReactiveFormsModule, FormsModule],
   templateUrl: './step3.component.html',
   styleUrl: './step3.component.css'
 })
 export class Step3Component {
-
+  testScores = [
+    { name: 'GRE', selected: '', score: '' },
+    { name: 'GMAT', selected: '', score: '' },
+    { name: 'Duolingo', selected: '', score: '' },
+    { name: 'IELTS', selected: '', score: '' },
+    { name: 'TOEFL', selected: '', score: '' },
+    { name: 'PTE', selected: '', score: '' }
+  ];
 }
