6e15b7ef38f032627c34774f7850b5216db6da89
Ashish Ranjan <ashish2013@hotmail.com>
2025-02-05 21:24:18 +0530

    step4 and step5

--- src/app/loan-journey/step4/step4.component.ts
+++ src/app/loan-journey/step4/step4.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-step4',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './step4.component.html',
   styleUrl: './step4.component.css'
 })
 export class Step4Component {
+
+  admissionStatus='Applied';
 
 }
--- src/app/loan-journey/step5/step5.component.ts
+++ src/app/loan-journey/step5/step5.component.ts
@@ -8,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class Step5Component {
 
+  country = 'India';
 }
