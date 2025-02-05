03b5e1ad199cdec141735580dbf9d4af7a268400
APA Consultancy <aloksrivastav568@gmail.com>
2025-02-05 20:19:20 +0530

    step-7 added !!

--- src/app/loan-journey/step7/step7.component.ts
+++ src/app/loan-journey/step7/step7.component.ts
@@ -1,11 +1,44 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-step7',
-  imports: [],
   templateUrl: './step7.component.html',
-  styleUrl: './step7.component.css'
+  styleUrls: ['./step7.component.css'],
+  imports: [ReactiveFormsModule, CommonModule],
 })
-export class Step7Component {
+export class Step7Component implements OnInit {
+  questionnaireForm: FormGroup;
+  months: string[] = [];
+  loanAmounts: string[] = ['Upto 12 Lakhs', '10-20 Lakhs', '20-50 Lakhs', '50-1cr', 'Above 1 cr'];
+  submitted = false;
 
+  constructor(private fb: FormBuilder) {
+    this.questionnaireForm = this.fb.group({
+      intake: ['', Validators.required],
+      startMonth: ['', Validators.required],
+      loanAmount: ['', Validators.required],
+      loanType: ['', Validators.required]
+    });
+  }
+
+  ngOnInit(): void {
+    this.generateMonths();
+  }
+
+  generateMonths() {
+    const currentDate = new Date();
+    for (let i = 0; i < 12; i++) {
+      const month = new Date(currentDate.getFullYear(), currentDate.getMonth() + i, 1);
+      this.months.push(month.toLocaleString('default', { month: 'long', year: 'numeric' }));
+    }
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    if (this.questionnaireForm.valid) {
+      console.log(this.questionnaireForm.value);
+    }
+  }
 }
