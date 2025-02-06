c1facc79ef33713aab44a6f2b177fb803cbdb5c6
piyushashu <piyush976156@gmail.com>
2025-02-06 15:52:58 +0530

    step 5 and 9 created

--- src/app/loan-journey/step5/step5.component.ts
+++ src/app/loan-journey/step5/step5.component.ts
@@ -9,6 +9,24 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
   styleUrl: './step5.component.css'
 })
 export class Step5Component {
-  Universities = ['ABC','XYZ'];
-  country = sessionStorage.getItem('selectedCountry');
+  countries: string[] = [
+    'United States',
+    'United Kingdom',
+    'Canada',
+    'Australia',
+    'Germany',
+    'France',
+    'Japan',
+    'Singapore',
+    'New Zealand',
+    'Netherlands'
+  ];
+
+  selectedCountry: string = '';
+
+  onCountryChange() {
+    // You can add additional logic here if needed
+    console.log('Selected country:', this.selectedCountry);
+  }
+  // country = sessionStorage.getItem('selectedCountry');
 }
--- src/app/loan-journey/step9/step9.component.ts
+++ src/app/loan-journey/step9/step9.component.ts
@@ -1,11 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgSelectModule } from '@ng-select/ng-select';
+
 
 @Component({
   selector: 'app-step9',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule, FormsModule, NgSelectModule],
   templateUrl: './step9.component.html',
   styleUrl: './step9.component.css'
 })
 export class Step9Component {
+  selectedOption: string = '';
+  selectedBanks: string[] = [];
+  bankList: string[] = ['SBI', 'PNB', 'AXIS', 'Indusland','HDFC','ICICI','Credila','InCred','IDFC','Auxilo','Prodigy'];
+  onOptionChange() {
+    if (this.selectedOption === 'No') {
+      this.selectedBanks = [];
+    }
+  }
+
+  onBankSelectionChange() {
+    // Logic if needed on bank selection change
+  }
 
 }
