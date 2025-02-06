39636a00fb6fffcf4a7e985787e191d09bfc3174
piyushashu <piyush976156@gmail.com>
2025-02-06 23:45:15 +0530

    create step6

--- src/app/loan-journey/step6/step6.component.ts
+++ src/app/loan-journey/step6/step6.component.ts
@@ -1,11 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-step6',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './step6.component.html',
   styleUrl: './step6.component.css'
 })
-export class Step6Component {
+export class Step6Component implements OnInit {
+  degrees: string[] = [
+    'UG',
+    'PG',
+    'Certificate Program',
+    'Other'
+  ];
 
+  programNames: string[] = [
+    'STEM',
+    'Non STEM',
+    'Management',
+    'Other'
+  ];
+
+  courseDurations: number[] = [12, 24, 36];
+
+  selectedDegree: string = 'UG';
+  selectedProgramName: string = 'STEM';
+  selectedDuration: string = '12';
+
+  ngOnInit() {
+    // Optional: You can set default values or perform any initialization here
+    console.log('Degrees:', this.degrees);
+    console.log('Program Names:', this.programNames);
+    console.log('Course Durations:', this.courseDurations);
+  }
+
+  // Optional: Add methods to log selections
+  onDegreeChange() {
+    console.log('Selected Degree:', this.selectedDegree);
+  }
+
+  onProgramNameChange() {
+    console.log('Selected Program Name:', this.selectedProgramName);
+  }
+
+  onDurationChange() {
+    console.log('Selected Duration:', this.selectedDuration);
+  }
 }
