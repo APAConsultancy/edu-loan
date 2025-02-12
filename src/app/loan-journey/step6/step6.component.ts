d2a626c0b9cf8c4772d49e014695b645ffec4791
APA Consultancy <aloksrivastav568@gmail.com>
2025-02-12 21:41:45 +0530

    Changes !!

--- src/app/loan-journey/step6/step6.component.ts
+++ src/app/loan-journey/step6/step6.component.ts
@@ -51,12 +51,13 @@ export class Step6Component implements OnInit {
   ngOnInit() {
     if (this.sessionService) {
     const programDetails = this.sessionService.getItem('programDetails');
-    this.programForm.patchValue({
-      degree: this.programValue.degree?this.programValue.degree:'',
-      programName: this.programValue.programName?this.programValue.programName:'',
-      courseDuration: this.programValue.courseDuration?this.programValue.courseDuration:'',
-    });
     this.programValue = programDetails ? JSON.parse(programDetails) : this.programValue;
+    this.programForm.patchValue({
+      degree: this.programValue.degree? this.programValue?.degree:'',
+      programName: this.programValue.programName?this.programValue?.programName:'',
+      courseDuration: this.programValue.courseDuration?this.programValue?.courseDuration:'',
+    });
+    
     this.selectedDegree = this.programValue.degree ? this.programValue?.degree : '';
     this.selectedProgramName = this.programValue.programName ? this.programValue?.programName : '';
     this.selectedDuration = this.programValue.courseDuration ? this.programValue?.courseDuration : '';
