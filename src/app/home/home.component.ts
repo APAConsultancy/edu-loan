286a464e8fc97f67c30c3c1bf72bd472bc5faa10
kapilshrm <kd.discoveryindia@gmail.com>
2025-02-19 20:15:02 +0530

    trying to add for loop in home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
-  imports: [RouterOutlet, RouterLink, RouterLinkActive
+  imports: [RouterOutlet, RouterLink, RouterLinkActive,CommonModule
   ],
 })
-export class HomeComponent {
+export class HomeComponent implements  OnInit {
+  textBlocks: string[] = [
+    'First text section',
+    'Second text section',
+    'Third text section',
+    'Fourth text section'
+  ];
 
+  activeIndex = 0;
+
+  ngOnInit() {
+    this.startAutoSwitch();
+  }
+
+  startAutoSwitch() {
+    setInterval(() => {
+      this.activeIndex = (this.activeIndex + 1) % this.textBlocks.length;
+    }, 2000); // Switch every 2 seconds
+  }
 }
+
+
